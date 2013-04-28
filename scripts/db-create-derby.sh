5827b83c69450a707ae9622fdad4e4e9ae919be2
Andrzej Wójcik <andrzejw@tigase.org>
2013-04-28 19:05:26 +0200

    Fixed missing jar in classpath in db-create-derby.sh

--- scripts/db-create-derby.sh
+++ scripts/db-create-derby.sh
@@ -10,5 +10,5 @@
 
 java -Dij.protocol=jdbc:derby: -Dij.database="$2;create=true" \
 		-Dderby.system.home=`pwd` \
-		-cp $1/libs/derby.jar:$1/libs/derbytools.jar:$1/jars/tigase-server.jar \
+		-cp $1/libs/derby.jar:$1/libs/derbytools.jar:$1/libs/tigase-socks5.jar:$1/jars/tigase-server.jar \
 		org.apache.derby.tools.ij database/derby-schema.sql &> derby-db-create.txt

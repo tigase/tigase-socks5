ac4bd496655c92cdede7fe5f4fde5cfc81fa8bdf
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2013-04-29 11:54:58 +0200

    rename schema files

--- scripts/db-create-derby.sh
+++ scripts/db-create-derby.sh
@@ -11,4 +11,4 @@
 java -Dij.protocol=jdbc:derby: -Dij.database="$2;create=true" \
 		-Dderby.system.home=`pwd` \
 		-cp $1/libs/derby.jar:$1/libs/derbytools.jar:$1/libs/tigase-socks5.jar:$1/jars/tigase-server.jar \
-		org.apache.derby.tools.ij database/derby-schema.sql &> derby-db-create.txt
+		org.apache.derby.tools.ij database/derby-socks5-schema.sql &> derby-db-create.txt

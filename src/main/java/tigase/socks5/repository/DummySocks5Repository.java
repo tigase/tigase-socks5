9634156424c311c10770dd3766d07492e8cfdd67
Wojciech Kapcia <wojtek@tigase.org>
2019-01-21 16:13:15 -0300

    #8483 update licence headers

--- src/main/java/tigase/socks5/repository/DummySocks5Repository.java
+++ src/main/java/tigase/socks5/repository/DummySocks5Repository.java
@@ -1,8 +1,6 @@
-/*
- * DummySocks5Repository.java
- *
- * Tigase Jabber/XMPP Server - SOCKS5 Proxy
- * Copyright (C) 2004-2017 "Tigase, Inc." <office@tigase.com>
+/**
+ * Tigase Socks5 Component - SOCKS5 proxy component for Tigase
+ * Copyright (C) 2011 Tigase, Inc. (office@tigase.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by

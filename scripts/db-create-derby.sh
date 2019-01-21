1c75f531a2b6cc699cec5951cfaa8edb4f4068c5
Wojciech Kapcia <wojtek@tigase.org>
2019-01-21 16:13:53 -0300

    #8483 update licence headers

--- scripts/db-create-derby.sh
+++ scripts/db-create-derby.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# Tigase Socks5 Component - SOCKS5 proxy component for Tigase
+# Copyright (C) 2011 Tigase, Inc. (office@tigase.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. Look for COPYING file in the top folder.
+# If not, see http://www.gnu.org/licenses/.
+#
+
 
 [[ "$1" = "" ]] && \
   echo "Give me a path to the location where Tigase XMPP Server is installed" && \
--- src/main/java/tigase/socks5/Stream.java
+++ src/main/java/tigase/socks5/Stream.java
@@ -1,3 +1,20 @@
+/**
+ * Tigase Socks5 Component - SOCKS5 proxy component for Tigase
+ * Copyright (C) 2011 Tigase, Inc. (office@tigase.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ */
 package tigase.socks5;
 import java.io.IOException;
 import java.nio.ByteBuffer;
